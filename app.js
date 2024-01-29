bcca4f4dead38016e4b219ec1f0a6b17a1b8c4f8
amin43511 <154742693+amin43511@users.noreply.github.com>
2024-01-29 17:17:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const ptr = prompt("Tell me Your Percentage")
+
+if (ptr<= 50) {
+     console.log("Your are Pass. Phr bhi prh le bhai")
+     alert("You've Just Pass")
+ } else if(ptr<= 60){
+     console.log("Grade B")
+     alert("Good, Keep it Up.!")
+ }else if(ptr<= 70){
+     console.log("Grade A")
+     alert("Well Deserved.!")
+ }else if(ptr<= 80){
+     console.log("Grade A-1")
+     alert("Great Achievement")
+ }else if(ptr<= 90){
+     console.log("Grade A-1+")
+     alert("Fantastic.!")
+ }else if(ptr<= 100){
+        console.log("Excellent Work.!")
+        alert("Excellent Work.!")
+ }else{
+     console.log("Please Input the correct data")
+}
